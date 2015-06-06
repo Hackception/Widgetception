a20b5fb76e41f3d1b3f2d5f89100f7e7a16aa639
Austin Murdock <austin.d.murdock@gmail.com>
2015-06-06 01:26:06 -0500

    feat(create): adding in basic form fields

---
+++ client/app/create/create.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('lockerdomeApp')
+  .controller('CreateCtrl', function ($scope) {
+    /* Declarations */
+    $scope.model = {
+      data: {}
+    };
+    $scope.widgetTypes = ['lineChart'];
+
+    /* Initialization */
+
+  });
