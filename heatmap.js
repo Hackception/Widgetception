5c9af916cf4b5ed030da564ef87879f54f22c77f
Brian Schiller <bgschiller@gmail.com>
2015-06-06 14:14:31 -0500

    heatmap seems to be working

--- heatmap.js
+++ heatmap.js
@@ -21,7 +21,9 @@ The output will be like [
 
 The dimensions of this array shall be 400 rows by 600 cols
 */
-var interpolateUserLine = _.compose(sparseToDense, interpolate);
+function interpolateUserLine(userLine, xRange, yRange){
+  return sparseToDense(interpolate(userLine), xRange, yRange);
+}
 
 /*
 function interpolate(userLine)
@@ -34,7 +36,7 @@ We will target 400 rows, so we should cut that many x-slices.
 function interpolate(userLine){
   var xMin = userLine[0].x,
       xMax = userLine[userLine.length - 1].x,
-      numInter = WIDTH / (xMax - xMin) - 1,
+      numInter = WIDTH / (xMax - xMin),
       pts = [userLine[0]],
       start, end;
   for (var ux=1; ux < userLine.length; ux++){
@@ -57,25 +59,39 @@ function sparseToDense(sparse)
 Given an array [{x: 1, y:2}, {x:1.2, y:2.4}, ..]
 Produces a dense 2d array of the values.
 */
-function sparseToDense(sparse, yRange){
-  var dense = 2dArray(WIDTH, HEIGHT);
-  for (var ix = 0; ix < HEIGHT; ix++){
-    var jx = Math.round(
-      (sparse[ix].y - yRange[0]) / (yRange[1] - yRange[0]) * HEIGHT
+function sparseToDense(sparse, xRange, yRange){
+  var dense = dArray();
+  sparse.forEach(function(d){
+    var ix = Math.round(
+      (d.x - xRange[0]) / (xRange[1] - xRange[0]) * WIDTH
+    ),
+        jx = Math.round(
+      (d.y - yRange[0]) / (yRange[1] - yRange[0]) * HEIGHT
     );
-    dense[ix][jx] = 1;
-  }
+    dense[Math.max(Math.min(jx, HEIGHT - 1), 0)]
+         [Math.max(Math.min(ix, WIDTH - 1), 0)] = 1;
+  });
   return dense;
 }
 
 
-function 2dArray(w, h){
+function dArray(){
   var arr = []
-  for (var ix = 0; ix < h; ix++){
+  for (var ix = 0; ix < HEIGHT; ix++){
     arr.push([]);
-    for (var jx = 0; jx < w; jx++){
+    for (var jx = 0; jx < WIDTH; jx++){
       arr[ix].push(0);
     }
   }
   return arr;
+}
+
+function sumDenseArrays(a, b){
+  var c = dArray(WIDTH, HEIGHT);
+  for (var jx=0; jx < HEIGHT; jx++){
+    for (var ix=0; ix< WIDTH; ix++){
+      c[jx][ix] = a[jx][ix] + b[jx][ix];
+    }
+  }
+  return c;
 }
