0670086ae780331242abaf51147ddda1de4ca50a
Michael Stramel <Michael.Stramel@panerabread.com>
2015-06-06 12:38:12 -0500

    basic chart page

--- client/app/chart/chart.controller.js
+++ client/app/chart/chart.controller.js
@@ -1,18 +1,147 @@
 'use strict';
 
-function ChartCtrl() {
-  // private
+function ChartCtrl($http, $q, $stateParams) {
+  var accountId = $stateParams.accountId;
+  var appId = $stateParams.appId;
+  var idUrl = $stateParams.idUrl;
+  var loginToken = $stateParams.loginToken;
+  var args = JSON.parse($stateParams.args);
+  var contentId = _.get(args, 'id');
+  var operation = _.get(args, 'op');
 
+  var contentPromise, heatmapPromise;
+  var headline;
+  var content;
+  var thumbUrl;
+  var xLabel, xMin, xMax, xStep;
+  var yLabel, yMin, yMax, yStep;
+  var trueLine;
+
+  var showResults = false;
+  var showHeatmap = false;
+  var errors = [];
+
+  // private
+  function init() {
+    if (operation === 'render_content') {
+      var url = '/content?' + escape(JSON.stringify({
+        app_id: appId,
+        content_id: contentId
+      }));
+      contentPromise = $http.get(url)
+        .then(function (data) {
+
+          if (!data.status) {
+            return $q.reject(data.error_message);
+          }
+
+          var appData = data.result.app_data;
+          headline = data.result.name;
+          content = data.result.text;
+          thumbUrl = data.result.thumb_url;
+          xLabel = appData.xAxisLabel;
+          xMin = appData.xAxisMin;
+          xMax = appData.xAxisMax;
+          xStep = appData.yAxisStep;
+          yLabel = appData.yAxisLabel;
+          yMin = appData.yAxisMin;
+          yMax = appData.yAxisMax;
+          yStep = appData.yAxisStep;
+          trueLine = appData.trueLine;
+
+        }, function (data) {
+          return $q.reject(data.error_message);
+        })
+        .catch(function (error) {
+          if (error) {
+            errors.push(error);
+          } else {
+            // errors.push('Unknown Error: Content Data Fetch');
+          }
+        });
+
+      // TODO: Fetch Heatmap Data
+      heatmapPromise = $http.get('')
+        .then(function (data) {
+          if (!data.status) {
+            return $q.reject(data.error_message);
+          }
+        }, function (data) {
+          return $q.reject(data.error_message);
+        })
+        .catch(function (error) {
+          if (error) {
+            errors.push(error);
+          } else {
+            // errors.push('Unknown Error: Heatmap Data Fetch');
+          }
+        });
+    }
+  }
 
   // public
+  function handleSubmission() {
+    // TODO: Post user data
+    $http.post('', {})
+      .then(function (data) {
+        if (!data.status) {
+          return $q.reject(data.error_message);
+        }
 
+        showResults = true;
+        showHeatmap = true;
+
+      }, function (data) {
+        return $q.reject(data.error_message);
+      })
+      .catch(function (error) {
+        if (error) {
+          errors.push(error);
+        } else {
+          errors.push('Unknown Error: Data Submission');
+        }
+      });
+  }
+
+  function handleClear() {
+    // TODO: Clear Data
+  }
 
   // exports
   angular.extend(this, {
 
+    // Content Data
+    contentRender: contentPromise,
+    heatmapRender: heatmapPromise,
+    headline: headline,
+    content: content,
+    thumbUrl: thumbUrl,
+    xLabel: xLabel,
+    xMin: xMin,
+    xMax: xMax,
+    xStep: xStep,
+    yLabel: yLabel,
+    yMin: yMin,
+    yMax: yMax,
+    yStep: yStep,
+    trueLine: trueLine,
+
+    showResults: showResults,
+    showHeatmap: showHeatmap,
+    errors: errors,
+    handleSubmission: handleSubmission,
+    handleClear: handleClear
+
   });
+
+  init();
 }
 
 angular
   .module('lockerdomeApp')
-  .controller('ChartCtrl', ChartCtrl);
+  .controller('ChartCtrl', [
+    '$http',
+    '$q',
+    '$stateParams',
+    ChartCtrl
+  ]);
--- client/app/chart/chart.js
+++ client/app/chart/chart.js
@@ -6,6 +6,24 @@ angular.module('lockerdomeApp')
       .state('chart', {
         url: '/bacon?account_id&app_id&Id_url&login_token&args',
         templateUrl: 'app/chart/chart.html',
-        controller: 'ChartCtrl'
+        controller: 'ChartCtrl',
+        controllerAs: 'vm',
+        resolve: {
+          accountId: ['$stateParams', function ($stateParams) {
+            return $stateParams.account_id;
+          }],
+          appId: ['$stateParams', function ($stateParams) {
+            return $stateParams.app_id;
+          }],
+          idUrl: ['$stateParams', function ($stateParams) {
+            return $stateParams.Id_url;
+          }],
+          loginToken: ['$stateParams', function ($stateParams) {
+            return $stateParams.login_token;
+          }],
+          args: ['$stateParams', function ($stateParams) {
+            return $stateParams.args;
+          }]
+        }
       });
   });
