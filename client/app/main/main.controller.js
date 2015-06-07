74ca9c7a0eac3e654e719ac613e4db9d7834ab3f
Austin Murdock <austin.d.murdock@gmail.com>
2015-06-06 21:41:28 -0500

    feat(main): adding in a basic entry page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,5 +1,76 @@
 'use strict';
 
 angular.module('lockerdomeApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $state) {
+    /* Declarations */
+    $scope.articles = [
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      },
+      {
+        title: 'Daily Facebook Usage vs Overall Happiness',
+        content: 'We can write a sweet article here with some awesome <i>embedded</i> <code>HTML</code> tags that will ' +
+          'get rendered properly.  Since we are running an AngularJS framework for our <sub>front-end</sub> we can just' +
+          ' drop these articles into a javascript array and they will render correctly.  This also means we could grab ' +
+          'the content from an API if we chose to.'
+      }
+    ];
+
+    /* Functions */
+    $scope.goToCreate = function () {
+      $state.go('create');
+    };
   });
