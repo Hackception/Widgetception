58d03c2e3dbc2b6618d26ee6e461b49606b27bf8
Brian Schiller <bgschiller@gmail.com>
2015-06-07 10:28:52 -0500

    dial back the width requirements

--- client/app/d3Chart/d3Chart.directive.js
+++ client/app/d3Chart/d3Chart.directive.js
@@ -338,7 +338,7 @@ function d3Chart($timeout, $window) {
     var handleResize = _.debounce(function() {
       theChart
         .height(Math.min(500, window.innerHeight - 100))
-        .width(Math.min(600, window.innerWidth - 100));
+        .width(Math.min(600, window.innerWidth - 30));
 
       d3.select('d3-chart').call(theChart);
     }, 350);
